fae7e828e13f262700443471a9ed80e64dc87f90
clam <lam.clarence.bc@gmail.com>
2022-04-06 21:01:30 -0700

    firstcommit

---
+++ frontend/src/App.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      todoList:[],
+      activeItem:{
+        id: null,
+        title: '',
+        completed: false,
+      },
+      editing:false,
+    }
+
+    // gives us access to "this" method within fetchTasks function
+    this.fetchTasks = this.fetchTasks.bind(this)
+  }
+
+  // "Lifecycle method"
+  componentWillMount(){
+    this.fetchTasks()
+  }
+
+  fetchTasks(){
+    console.log('Fetching... ')
+
+    fetch('http://127.0.0.1:8000/api/task-list/')
+    .then(response => response.json())
+    .then(data => 
+      console.log('Data', data)
+      )
+  }
+
+  render() {
+
+    return (
+      <div className="container">
+        <div id="task-container">
+
+          <div id="form-wrapper">
+            <form id="form">
+              <div className="flex-wrapper">
+                <div style={{ flex: 6 }}>
+                  <input className="form-control" id="title" type="text" name="title" placeholder="add task here">
+                  </input>
+                </div>
+
+                <div style={{ flex: 1 }}>
+                  <input id="submit" className="btn btn-warning" type="submit" name="Add" ></input>
+                </div>
+
+              </div>
+            </form>
+          </div>
+
+          <div id="list-wrapper">
+
+          </div>
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
