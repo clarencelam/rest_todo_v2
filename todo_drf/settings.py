4fb272beed3849365d09429662afe2ebc35be20f
clam <lam.clarence.bc@gmail.com>
2022-04-12 01:19:04 -0700

    fix cors header issue

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -65,7 +65,9 @@ ROOT_URLCONF = 'todo_drf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend/build')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,6 +132,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS =  [
+    os.path.join(BASE_DIR, 'frontend/build/static')
+]
+
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "http://localhost:8000",
 ]
