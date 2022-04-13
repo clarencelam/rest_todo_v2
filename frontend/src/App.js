36caf370d81a7a131301ea0cf7dd66d1a85d9044
clam <lam.clarence.bc@gmail.com>
2022-04-12 22:53:11 -0700

    add todo checkbox

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,202 +26,207 @@ class App extends React.Component {
   };
 
   getCookie(name) {
-  let cookieValue = null;
-  if (document.cookie && document.cookie !== '') {
-    const cookies = document.cookie.split(';');
-    for (let i = 0; i < cookies.length; i++) {
-      const cookie = cookies[i].trim();
-      // Does this cookie string begin with the name we want?
-      if (cookie.substring(0, name.length + 1) === (name + '=')) {
-        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-        break;
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+      const cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) === (name + '=')) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
       }
     }
-  }
-  return cookieValue;
-}
-
-// "Lifecycle method"
-componentWillMount() {
-  this.fetchTasks()
-}
-
-// Component which fetches the latest todoList data from backend
-fetchTasks() {
-  console.log('Fetching... ')
-
-  fetch('http://127.0.0.1:8000/api/task-list/')
-    .then(response => response.json())
-    .then(data =>
-      this.setState({
-        todoList: data,
-      })
-    )
-}
-
-// Function to listen for change events
-handleChange(e){
-  var name = e.target.name
-  var value = e.target.value
-  console.log('Name:', name)
-  console.log('Value:', value)
-
-  this.setState({
-    activeItem: {
-      ...this.state.activeItem,
-      title: value
-    }
-  })
-}
-
-// Component to handle TODO item submissions
-handleSubmit(e){
-  e.preventDefault()
-  console.log('ACTIVE ITEM:', this.state.activeItem)
-
-  var csrftoken = this.getCookie('csrftoken')
-
-  // We want to send POST data to another URL (backend)
-  var url = 'http://localhost:8000/api/task-create/'
-
-  if(this.state.editing==true){
-    url = `http://localhost:8000/api/task-update/${this.state.activeItem.id}/`
-    this.setState({
-      editing:false
-    })
+    return cookieValue;
   }
 
-  fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-      'X-CSRFToken': csrftoken,
-    },
-    body: JSON.stringify(this.state.activeItem)
-  }).then((response) => {
-    this.fetchTasks() // Fetch the updated list of tasks
+  // "Lifecycle method"
+  componentWillMount() {
+    this.fetchTasks()
+  }
+
+  // Component which fetches the latest todoList data from backend
+  fetchTasks() {
+    console.log('Fetching... ')
+
+    fetch('http://127.0.0.1:8000/api/task-list/')
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          todoList: data,
+        })
+      )
+  }
+
+  // Function to listen for change events
+  handleChange(e) {
+    var name = e.target.name
+    var value = e.target.value
+    console.log('Name:', name)
+    console.log('Value:', value)
+
     this.setState({
       activeItem: {
-        id: null,
-        title: '',
-        completed: false,
+        ...this.state.activeItem,
+        title: value
       }
     })
-  }).catch(function (error) {
-    console.log('ERROR: ', error)
-  })
-}
+  }
 
-startEdit(task){
-  this.setState({
-    activeItem:task,
-    editing:true,
-  })
-}
+  // Component to handle TODO item submissions
+  handleSubmit(e) {
+    e.preventDefault()
+    console.log('ACTIVE ITEM:', this.state.activeItem)
 
-deleteItem(task){
-  var csrftoken = this.getCookie('csrftoken')
+    var csrftoken = this.getCookie('csrftoken')
 
-  fetch(`http://localhost:8000/api/task-delete/${task.id}/`, {
-    method:'DELETE',
-    headers:{
-      'Content-type':'application/json',
-      'X-CSRFToken':csrftoken,
-    },
-  }).then((response) =>{
-    this.fetchTasks()
-  })
-}
+    // We want to send POST data to another URL (backend)
+    var url = 'http://localhost:8000/api/task-create/'
 
-strikeUnstrike(task){
-  task.completed = !task.completed
-
-  var csrftoken = this.getCookie('csrftoken')
-  var url = `http://localhost:8000/api/task-update/${task.id}/`
+    if (this.state.editing == true) {
+      url = `http://localhost:8000/api/task-update/${this.state.activeItem.id}/`
+      this.setState({
+        editing: false
+      })
+    }
 
     fetch(url, {
       method: 'POST',
       headers: {
-        'Content-type':'application/json',
-        'X-CSRFToken':csrftoken,
+        'Content-type': 'application/json',
+        'X-CSRFToken': csrftoken,
       },
-      body:JSON.stringify({'completed':task.completed,'title':task.title})
-    }).then(()=> {
+      body: JSON.stringify(this.state.activeItem)
+    }).then((response) => {
+      this.fetchTasks() // Fetch the updated list of tasks
+      this.setState({
+        activeItem: {
+          id: null,
+          title: '',
+          completed: false,
+        }
+      })
+    }).catch(function (error) {
+      console.log('ERROR: ', error)
+    })
+  }
+
+  startEdit(task) {
+    this.setState({
+      activeItem: task,
+      editing: true,
+    })
+  }
+
+  deleteItem(task) {
+    var csrftoken = this.getCookie('csrftoken')
+
+    fetch(`http://localhost:8000/api/task-delete/${task.id}/`, {
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json',
+        'X-CSRFToken': csrftoken,
+      },
+    }).then((response) => {
+      this.fetchTasks()
+    })
+  }
+
+  strikeUnstrike(task) {
+    task.completed = !task.completed
+
+    var csrftoken = this.getCookie('csrftoken')
+    var url = `http://localhost:8000/api/task-update/${task.id}/`
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        'X-CSRFToken': csrftoken,
+      },
+      body: JSON.stringify({ 'completed': task.completed, 'title': task.title })
+    }).then(() => {
       this.fetchTasks()
     })
 
-  console.log('Task striked-out: ', task.completed)
-}
+    console.log('Task striked-out: ', task.completed)
+  }
 
-render() {
-  var tasks = this.state.todoList
-  var self = this // Allows the startEdit() call in the edit button to reference "this" 
+  renderAllTasks = () => {
+    // Component which returns and renders the list of all tasks
+    var tasks = this.state.todoList
+    var self = this // Allows the function calls to reference "this" 
 
-  return (
-    <div className="container">
-      <div id="task-container">
+    return (
+      // For each task in the todoList, assign the key to the todo index, print them all
+      tasks.map(function (task, index) {
+        return (
+          <div key={index} className="task-wrapper flex-wrapper"
+          onClick={() => self.strikeUnstrike(task)}>
 
-        <div id="form-wrapper">
-
-          <form onSubmit={this.handleSubmit} id="form">
-
-            <div className="flex-wrapper">
-              <div style={{ flex: 6 }}>
-                <input onChange={this.handleChange}
-                  className="form-control" id="title" value={this.state.activeItem.title} type="text" name="title" placeholder="add task here">
-                </input>
-              </div>
-
-              <div style={{ flex: 1 }}>
-                <input id="submit" className="btn btn-warning" type="submit" name="Add" ></input>
-              </div>
-
+            <div style={{ flex: 7 }}>
+              {task.completed == false ? (
+                <span>☐ {task.title}</span>
+              ) : (
+                <span>☑ <strike> {task.title}</strike></span>
+              )}
             </div>
-          </form>
 
-        </div>
+            <div style={{ flex: 1 }}>
+              <button
+                className="btn btn-sm btn-outline-info"
+                onClick={() => self.startEdit(task)}>Edit</button>
+            </div>
 
-        <div id="list-wrapper">
-          {
-            // For each task in the todoList, assign the key to the todo index, print them all
-            tasks.map(function (task, index) {
-              return (
-                <div key={index} className="task-wrapper flex-wrapper">
+            <div style={{ flex: 1 }}>
+              <button
+                className="btn btn-sm btn-outline-dark delete"
+                onClick={() => self.deleteItem(task)}>-</button>
+            </div>
 
-                  <div 
-                  onClick={() => self.strikeUnstrike(task)}
-                  style={{ flex: 7 }}>
+          </div>
+        )
+      })
+    )
+  }
 
-                    {task.completed == false ? (
-                    <span>{task.title}</span>
-                    ):(
-                      <strike>{task.title}</strike>
-                    )
-                    }
-                  </div>
+  render() {
 
-                  <div style={{ flex: 1 }}>
-                    <button 
-                    className="btn btn-sm btn-outline-info"
-                    onClick={() => self.startEdit(task)}>Edit</button>
-                  </div>
+    return (
+      <div className="container">
+        <div id="task-container">
 
-                  <div style={{ flex: 1 }}>
-                    <button 
-                    className="btn btn-sm btn-outline-dark delete"
-                    onClick={() => self.deleteItem(task)}>-</button>
-                  </div>
+          <div id="form-wrapper">
 
+            <form onSubmit={this.handleSubmit} id="form">
 
+              <div className="flex-wrapper">
+                <div style={{ flex: 6 }}>
+                  <input onChange={this.handleChange}
+                    className="form-control" id="title" value={this.state.activeItem.title} type="text" name="title" placeholder="add task here">
+                  </input>
                 </div>
-              )
-            })}
-        </div>
 
+                <div style={{ flex: 1 }}>
+                  <input id="submit" className="btn btn-warning" type="submit" name="Add" ></input>
+                </div>
+
+              </div>
+            </form>
+
+          </div>
+
+          <div id="list-wrapper">
+            {
+              this.renderAllTasks()
+            }
+          </div>
+
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
 }
 
 export default App;
